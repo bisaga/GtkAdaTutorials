5d40951da003f28566ac305e0f8821c72c43ad6c
bisaga <igor.babic@bisaga.com>
2016-08-05 00:01:17 +0200

    initial checkin

---
+++ src/gtkadatutorials.adb
@@ -0,0 +1,26 @@
+pragma Ada_2012;
+with Gtk.Main;
+with Gtk.Enums; use Gtk.Enums;
+
+with Main_Windows; use Main_Windows;
+
+
+
+procedure GtkAdaTutorials is
+   Win: Main_Windows.Main_Window;
+
+begin
+   --  Initialize GtkAda.
+   Gtk.Main.Init;
+
+   Main_Windows.Gtk_New (Win);
+   Win.Set_Position (Win_Pos_Center);
+   Win.Set_Title("Gtk+3 Ada tutorials");
+   Main_Windows.Show_All (Win);
+
+   --  Start the Gtk+ main loop
+   Gtk.Main.Main;
+
+end GtkAdaTutorials;
+
+
